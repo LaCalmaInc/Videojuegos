57c0df11846285cc359e7a0783b59589341aee5a
LaCalmaInc <alvaro.guzmans@usm.cl>
2025-04-10 23:10:07 -0400

    add light👍

--- First Game Test/Assets/_Scripts/Movement/Rotator.cs
+++ First Game Test/Assets/_Scripts/Movement/Rotator.cs
@@ -5,16 +5,17 @@ namespace TopDown.Movement
     public class Rotator : MonoBehaviour
     {
         protected void LookAt(Transform rotatedTransform, Vector3 target)
-        {
-            float lookAngle = AngleBetweenTwoPoints(transform.position, target) - 90;
-
-            rotatedTransform.eulerAngles = new Vector3(0, 0, lookAngle);
-        }
+            {
+                // Cambia la llamada a AngleBetweenTwoPoints para que use la dirección (b - a)
+                float lookAngle = AngleBetweenTwoPoints(rotatedTransform.position, target) - 90;
+                rotatedTransform.eulerAngles = new Vector3(0, 0, lookAngle);
+            }
 
         private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-        {
-            return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-        }
+            {
+                return Mathf.Atan2(b.y - a.y, b.x - a.x) * Mathf.Rad2Deg;
+            }
+
     }
 }
 
